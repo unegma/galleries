4870c5e8a2cf0ac3d9d3cbd734f71cad1564c4d0
Timothy Coleman <timhc22@gmail.com>
2022-09-21 01:09:31 +0100

    Start adding description

--- src/components/layout/RightSideDrawer.tsx
+++ src/components/layout/RightSideDrawer.tsx
@@ -11,57 +11,79 @@ import Typography from "@mui/material/Typography";
 import {ChevronLeft, ChevronRight, InfoOutlined} from "@mui/icons-material";
 
 export default function RightSideDrawer(
-  {infoOpen, toggleRightSideDrawer, setDrawerRightOpen, drawerRightOpen}:
-    {infoOpen: any, toggleRightSideDrawer: any, setDrawerRightOpen: Function, drawerRightOpen: boolean}) {
+  {infoOpen, toggleRightSideDrawer, setDrawerRightOpen, drawerRightOpen, infoTitle = 'The Title', infoText = 'This is info about this piece'}: any) {
 
   return (
-    <Drawer
-      className="rightDrawer"
-      open={drawerRightOpen}
-      onClose={(event:any) => {toggleRightSideDrawer(event)}}
-      anchor={'right'}
-    >
-      <Box
-        component="div"
-        sx={{ width: '50vw'}}
-        role="presentation"
-        onClick={(event: any) => {toggleRightSideDrawer(event)}}
-        onKeyDown={(event: any) => {toggleRightSideDrawer(event)}}
+    <>
+      <Drawer
+        className="rightDrawer"
+        open={drawerRightOpen}
+        onClose={(event:any) => {toggleRightSideDrawer(event)}}
+        anchor={'right'}
       >
+        <Box
+          component="div"
+          sx={{ width: '50vw'}}
+          role="presentation"
+          onClick={(event: any) => {toggleRightSideDrawer(event)}}
+          onKeyDown={(event: any) => {toggleRightSideDrawer(event)}}
+        >
 
-        {/*<List>*/}
+          {/*<List>*/}
 
-        {/*  <Link to="/" className="drawer-link">*/}
-        {/*    <ListItem key={'nav'} disablePadding>*/}
-        {/*      <Typography className={`main-title main-title-navbar`} variant="h6" component="div" sx={{ flexGrow: 1 }}*/}
-        {/*                  onClick={(event:any) => {toggleRightSideDrawer(event)}}>*/}
-        {/*        {process.env.REACT_APP_NAV_TITLE}*/}
-        {/*      </Typography>*/}
-        {/*    </ListItem>*/}
-        {/*  </Link>*/}
-        {/*</List>*/}
+          {/*  <Link to="/" className="drawer-link">*/}
+          {/*    <ListItem key={'nav'} disablePadding>*/}
+          {/*      <Typography className={`main-title main-title-navbar`} variant="h6" component="div" sx={{ flexGrow: 1 }}*/}
+          {/*                  onClick={(event:any) => {toggleRightSideDrawer(event)}}>*/}
+          {/*        {process.env.REACT_APP_NAV_TITLE}*/}
+          {/*      </Typography>*/}
+          {/*    </ListItem>*/}
+          {/*  </Link>*/}
+          {/*</List>*/}
 
-        {/*<Divider/>*/}
+          {/*<Divider/>*/}
 
-        {/*<div className={`buttons-container buttons-container--right`}>*/}
-        {/*  <InfoOutlined className="pointer" style={{ color: "black", margin: "0 4px" }} onClick={(event) => {toggleRightSideDrawer(event)}}/>*/}
-        {/*</div>*/}
+          {/*<div className={`buttons-container buttons-container--right`}>*/}
+          {/*  <InfoOutlined className="pointer" style={{ color: "black", margin: "0 4px" }} onClick={(event) => {toggleRightSideDrawer(event)}}/>*/}
+          {/*</div>*/}
 
-        <div className="buttons-container">
-          {/*<InfoOutlined className="pointer" style={{ color: "black", margin: "0 4px" }} onClick={() => {setShowInfoModal(!showInfoModal)}}/>*/}
+          <div className="buttons-container">
+            {/*<InfoOutlined className="pointer" style={{ color: "black", margin: "0 4px" }} onClick={() => {setShowInfoModal(!showInfoModal)}}/>*/}
 
-          <div className="pointer" onClick={(event) => {toggleRightSideDrawer(event)}}>
-            <InfoOutlined className="pointer" style={{ color: "black", margin: "0 4px" }} />
-            { drawerRightOpen && (
-              <ChevronRight style={{ color: "black", margin: "0 4px" }} />
-            )}
-            { !drawerRightOpen && (
-              <ChevronLeft style={{ color: "black", margin: "0 4px" }} />
-            )}
+            <div className="pointer" onClick={(event) => {toggleRightSideDrawer(event)}}>
+              <InfoOutlined className="pointer" style={{ color: "black", margin: "0 4px" }} />
+              { drawerRightOpen && (
+                <ChevronRight style={{ color: "black", margin: "0 4px" }} />
+              )}
+              { !drawerRightOpen && (
+                <ChevronLeft style={{ color: "black", margin: "0 4px" }} />
+              )}
+            </div>
           </div>
-        </div>
 
-      </Box>
-    </Drawer>
+          <div className="rightDrawer-text-container">
+            <Typography className="rightDrawer-text-container__title">
+              {infoTitle}
+            </Typography>
+            <Typography className="rightDrawer-text-container__description">
+              {infoText}
+            </Typography>
+          </div>
+
+        </Box>
+      </Drawer>
+
+      {/*hacky way to avoid the opacity*/}
+      { drawerRightOpen && (
+        <div className="rightDrawer-text-container">
+          <Typography className="rightDrawer-text-container__title">
+            {infoTitle}
+          </Typography>
+          <Typography className="rightDrawer-text-container__description">
+            {infoText}
+          </Typography>
+        </div>
+      ) }
+    </>
   )
 }
