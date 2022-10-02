3dd2860a11b205cd2d0ce29ad6dd68b72a595af2
Timothy Coleman <timhc22@gmail.com>
2022-10-02 19:42:32 +0100

    Fix bug

--- src/helpers/opcodeData.ts
+++ src/helpers/opcodeData.ts
@@ -1,9 +1,26 @@
 import * as rainSDK from "rain-sdk";
-import {BigNumber, ethers} from "ethers";
+import {BigNumber, BytesLike, ethers} from "ethers";
 import {StateConfig} from "rain-sdk";
-import {concat} from "ethers/lib/utils";
-import {op} from "rain-sdk/dist/utils";
+import { concat, Hexable, hexlify, zeroPad } from "ethers/lib/utils";
 import {Opcode} from "./web3Functions";
+
+export function op(code: number, erand = 0): Uint8Array {
+  return concat([bytify(code), bytify(erand)]);
+}
+
+/**
+ * Converts a value to raw bytes representation. Assumes `value` is less than or equal to 1 byte, unless a desired `bytesLength` is specified.
+ *
+ * @param value - value to convert to raw bytes format
+ * @param bytesLength - (defaults to 1) number of bytes to left pad if `value` doesn't completely fill the desired amount of memory. Will throw `InvalidArgument` error if value already exceeds bytes length.
+ * @returns {Uint8Array} - raw bytes representation
+ */
+export function bytify(
+  value: number | BytesLike | Hexable,
+  bytesLength = 1
+): BytesLike {
+  return zeroPad(hexlify(value), bytesLength);
+}
 
 export const maxCapForWallet = (cap: BigNumber): StateConfig => {
   return {
